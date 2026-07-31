da4ae479cf67addd844c84b9720aaba0631482bd
Oluwadamilola Dolapo <Dhorllar98@gmail.com>
2026-07-31 02:15:47 -0700

    feat: migrate to postgresql, add database auto-seeding, and restrict manager self-approval

--- LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
+++ LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
@@ -5,5 +5,6 @@ public class CreateLeaveRequestDto
     public Guid LeaveTypeId { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+    public string Reason { get; set; } = string.Empty; 
     public string? RequestComments { get; set; }
 }
