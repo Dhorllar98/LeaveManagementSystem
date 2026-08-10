d4c12f3305bafa4540281164dec5241ce9c09cf7
Oluwadamilola Dolapo <Dhorllar98@gmail.com>
2026-08-10 08:04:53 -0700

    Refactor backend roles to Employee, TeamLead, and HR; clean up DTOs and LeaveRequestsController

---
+++ LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestFilterDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.DTOs.LeaveRequest
+{
+    internal class LeaveRequestFilterDto
+    {
+    }
+}
